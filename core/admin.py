e68883218032ff1b768915afe2e0d8f1ad54289d
Jonathas Veras <Jonathasveras42@gmail.com>
2022-04-18 20:06:11 -0300

    mudando tabela no admin
    Alterando código para que cada elemento do model fique visível em uma tabela no /admin

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Product, Client
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preço', 'estoque')
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Client)
