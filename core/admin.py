a9fb54bc58e247fc83a90b42ce4faac67fc7a66d
Jonathas Veras <Jonathasveras42@gmail.com>
2022-04-18 20:20:21 -0300

    Adição do Cliente mais detalhado na tabela

--- core/admin.py
+++ core/admin.py
@@ -4,8 +4,12 @@ from .models import Product, Client
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'preço', 'estoque')
+    list_display = ('name', 'price', 'inventory')
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_name', 'email')
 
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Client)
+admin.site.register(Client, ClientAdmin)
